ea15b63c9856f72382f0360b26ee83c72c64c656
svetovid <divotevs@gmail.com>
2024-05-06 21:07:47 +0200

    ui controls

--- calculator-ui/src/app.rs
+++ calculator-ui/src/app.rs
@@ -4,9 +4,27 @@ use yew::prelude::*;
 pub fn app() -> Html {
     html! {
         <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+            <div class="panel">
+                <div class="formula-container">
+                    <label for="formula">{ "Formula" }</label>
+                    <input id="formula" type="text" class="control control-lg" required=true />
+                </div>
+                <div class="inputs-container">
+                    <label for="start">{ "Range of inputs" }</label>
+                    <input id="start" type="number" class="control control-sm" required=true />
+                    <input id="end" type="number" class="control control-sm" required=true />
+                </div>
+                <div>
+                    <button class="btn btn-sm">{ "Go!" }</button>
+                </div>
+            </div>
+            <div class="canvas">
+                <svg width=600 height=600>
+                    <g stroke="#007bff">
+                        <path d="M 0 0 L 100 100"></path>
+                    </g>
+                </svg>
+            </div>
         </main>
     }
 }
